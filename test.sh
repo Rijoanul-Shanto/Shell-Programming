f53e9b7af0c4033c32e910a7de566ce96d3fb8f4
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2017-12-14 14:53:06 +0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,27 +1,35 @@
-echo "Enter how many process you have :"
+#!/bin/bash
+echo "Enter number of process"
 read n
-echo "Enter precess , AT , BT respectively"
-for((i=1;i<=n;i++))
+
+echo "Enter process , AT , BT , Priority"
+for (( i = 1; i <= n; i++ ))
 do
-	read p[$i];
-	read at[$i];
-	read bt[$i];
+	read p[$i]
+	read at[$i]
+	read bt[$i]
+	read pr[$i]
 done
 
-for((i=1;i<=n-1;i++))
+for(( i = 1; i <= n-1 ; i++))
 do
-	for((j=1;j<=n-1;j++))
+	for(( j = 1; j <= n-i; j++))
 	do
-    	if [ ${at[$j]} -gt ${at[$j+1]} ]
-    	then
+	    if [ ${at[$j]} -gt ${at[$j+1]} ]
+	    then
         	temp=${at[$j]}
         	at[$j]=${at[$j+1]}
         	at[$j+1]=$temp
-        	
+    	
         	temp=${bt[$j]}
             bt[$j]=${bt[$j+1]}
             bt[$j+1]=$temp
+        
+        	temp=${pr[$j]}
+            pr[$j]=${pr[$j+1]}
+            pr[$j+1]=$temp
         	
+        
         	temp=${p[$j]}
             p[$j]=${p[$j+1]}
             p[$j+1]=$temp
@@ -33,28 +41,33 @@ ct=`expr ${at[1]} + ${bt[1]} `
 tat=`expr $ct - ${at[1]} `
 wt[1]=`expr $tat - ${bt[1]} `
 
-for((i=2;i<=n-1;i++))
+for(( i=2; i<=n-1; i++ ))
 do
-        for((j=2;j<=n-1;j++))
-        do
-            if [ ${bt[$j]} -gt ${bt[$j+1]} ]
-            then
-                temp=${at[$j]}
-                at[$j]=${at[$j+1]}
-                at[$j+1]=$temp
-        
-                temp=${bt[$j]}
-                bt[$j]=${bt[$j+1]}
-                bt[$j+1]=$temp
-        
-                temp=${p[$j]}
-                p[$j]=${p[$j+1]}
-                p[$j+1]=$temp
-            fi
-        done
+    for(( j=2; j<=n-1; j++ ))
+    do
+        if [ ${pr[$j]} -gt ${pr[$j+1]} ]
+        then
+            
+            temp=${at[$j]}
+            at[$j]=${at[$j+1]}
+            at[$j+1]=$temp
+    
+            temp=${bt[$j]}
+            bt[$j]=${bt[$j+1]}
+            bt[$j+1]=$temp
+    
+            temp=${p[$j]}
+            p[$j]=${p[$j+1]}
+            p[$j+1]=$temp
+    
+    	    temp=${pr[$j]}
+            pr[$j]=${pr[$j+1]}
+            pr[$j+1]=$temp
+        fi
+    done
 done
 
-for((i=2;i<=n;i++))
+for(( i=2; i<=n; i++ ))
 do
 	ct=`expr $ct + ${bt[$i]} `
 	tat=`expr $ct - ${at[$i]} `
@@ -62,14 +75,14 @@ do
 done
 
 twt=0
-echo ${wt[*]}
 
-for((i=1;i<=n;i++))
+for(( i=1; i<=n; i++))
 do
     twt=`expr $twt + ${wt[i]} `
 done
+
 echo $twt
 
 awt=`expr $twt / $n `
 
-echo $awt
+echo "Average waiting time $awt"
