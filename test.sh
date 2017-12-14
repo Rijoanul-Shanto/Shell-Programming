2dcd1036e083f23bca2a41ea8b9ec65f94c89fb2
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2017-12-14 15:01:30 +0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,88 +1,113 @@
 #!/bin/bash
+
 echo "Enter number of process"
 read n
 
-echo "Enter process , AT , BT , Priority"
+echo "Enter process, arrival time and burst time"
 for (( i = 1; i <= n; i++ ))
 do
-	read p[$i]
-	read at[$i]
-	read bt[$i]
-	read pr[$i]
+    read p[$i]
+    read at[$i]
+    read bt[$i]
 done
 
-for(( i = 1; i <= n-1 ; i++))
-do
-	for(( j = 1; j <= n-i; j++))
-	do
-	    if [ ${at[$j]} -gt ${at[$j+1]} ]
-	    then
-        	temp=${at[$j]}
-        	at[$j]=${at[$j+1]}
-        	at[$j+1]=$temp
-    	
-        	temp=${bt[$j]}
-            bt[$j]=${bt[$j+1]}
-            bt[$j+1]=$temp
-        
-        	temp=${pr[$j]}
-            pr[$j]=${pr[$j+1]}
-            pr[$j+1]=$temp
-        	
-        
-        	temp=${p[$j]}
-            p[$j]=${p[$j+1]}
-            p[$j+1]=$temp
-	    fi
-	done
-done
+echo "Enter time quantam value:"
+read TQ
 
-ct=`expr ${at[1]} + ${bt[1]} `
-tat=`expr $ct - ${at[1]} `
-wt[1]=`expr $tat - ${bt[1]} `
-
-for(( i=2; i<=n-1; i++ ))
+for (( i = 1; i < n; i++))
 do
-    for(( j=2; j<=n-1; j++ ))
+    for (( j = 1; j <= n - i; j++ ))
     do
-        if [ ${pr[$j]} -gt ${pr[$j+1]} ]
+        if (( at[j] > at[j+1] ))
         then
-            
             temp=${at[$j]}
-            at[$j]=${at[$j+1]}
+            at[j]=${at[$j+1]}
             at[$j+1]=$temp
-    
-            temp=${bt[$j]}
-            bt[$j]=${bt[$j+1]}
-            bt[$j+1]=$temp
-    
+            
             temp=${p[$j]}
-            p[$j]=${p[$j+1]}
+            p[j]=${p[$j+1]}
             p[$j+1]=$temp
-    
-    	    temp=${pr[$j]}
-            pr[$j]=${pr[$j+1]}
-            pr[$j+1]=$temp
+            
+            temp=${bt[$j]}
+            bt[j]=${bt[$j+1]}
+            bt[$j+1]=$temp
         fi
     done
 done
 
-for(( i=2; i<=n; i++ ))
+for (( i = 1; i <= n; i++ ))
 do
-	ct=`expr $ct + ${bt[$i]} `
-	tat=`expr $ct - ${at[$i]} `
-	wt[i]=`expr $tat - ${bt[$i]} `
+    echo "${p[$i]} ${at[$i]} ${bt[$i]}"
+    echo
 done
 
-twt=0
+point=1
+pos=1
+Q[1]=1
+gantt=0
 
-for(( i=1; i<=n; i++))
+for (( i = 1; i <= n; i++ ))
 do
-    twt=`expr $twt + ${wt[i]} `
+    mark[$i]=0
 done
 
-echo $twt
+mark[1]=1
 
-awt=`expr $twt / $n `
 
-echo "Average waiting time $awt"
+for (( i = 1; ; i++ ))
+do
+    if (( bt[Q[point]] >= TQ ))
+    then
+        gantt=`expr $gantt + $TQ`
+        bt[${Q[$point]}]=`expr ${bt[${Q[$point]}]} - $TQ`
+        
+        for (( j = 1; j <= n; j++ ))
+        do
+            if (( at[p[j]] <= gantt && mark[j] == 0 ))
+            then
+                pos=`expr $pos + 1`
+                Q[$pos]=$j
+                mark[${Q[$pos]}]=1
+            fi
+        done
+        
+        if (( bt[Q[point]] > 0 ))
+        then
+            pos=`expr $pos + 1`   
+            Q[$pos]=${Q[$point]}
+        fi
+        point=`expr $point + 1`
+    else
+        gantt=`expr $gantt + ${bt[${Q[$point]}]}`
+        bt[${Q[$point]}]=0
+        
+        for (( j = 1; j <= n; j++ ))
+        do
+            if (( at[p[j]] <= gantt && mark[j] != 0 ))
+            then
+                pos=`expr $pos + 1`
+                Q[$pos]=$j
+                mark[${Q[$pos]}]=1
+            fi
+        done
+        point=`expr $point + 1`
+    fi
+    
+    flag=0
+    
+    for (( k = 1; k <= n; k++ ))
+    do
+        if (( bt[k] != 0 ))
+        then
+            flag=1
+        fi
+    done
+    
+    if (( flag == 0 ))
+    then
+        break
+    fi
+    
+done
+
+echo "Final Gantt char value: $gantt"
